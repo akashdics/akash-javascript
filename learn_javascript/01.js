5f366f169b03a9714961cf175f795cad56efbded
akashdics <53614200+akashdics@users.noreply.github.com>
2024-06-26 06:03:00 +0000

    toaday

---
+++ learn_javascript/01.js
@@ -0,0 +1,11 @@
+const accountId = 161660
+let accountName ="AKASH SOLANKI";
+var accountType ="Saving";
+
+accountCity="Delhi"
+
+accountState ="New Delhi"
+
+
+
+console.table([accountId, accountType, accountCity, accountState, accountName])
